edcd7f71efdfbfbb4070545d9b5def08557a4c85
paxpor <pporter@byupathway.edu>
2026-05-21 11:37:26 -0600

    completing week03 assignments.

--- week03/code/Maze.cs
+++ week03/code/Maze.cs
@@ -32,38 +32,33 @@ public class Maze
     /// </summary>
     public void MoveLeft()
     {
-        // FILL IN CODE
+        if (_mazeMap[(_currX, _currY)][0])
+            _currX--;
+        else
+            throw new InvalidOperationException("Can't go that way!");
     }
 
-    /// <summary>
-    /// Check to see if you can move right.  If you can, then move.  If you
-    /// can't move, throw an InvalidOperationException with the message "Can't go that way!".
-    /// </summary>
     public void MoveRight()
     {
-        // FILL IN CODE
+        if (_mazeMap[(_currX, _currY)][1])
+            _currX++;
+        else
+            throw new InvalidOperationException("Can't go that way!");
     }
 
-    /// <summary>
-    /// Check to see if you can move up.  If you can, then move.  If you
-    /// can't move, throw an InvalidOperationException with the message "Can't go that way!".
-    /// </summary>
     public void MoveUp()
     {
-        // FILL IN CODE
+        if (_mazeMap[(_currX, _currY)][2])
+            _currY++;
+        else
+            throw new InvalidOperationException("Can't go that way!");
     }
 
-    /// <summary>
-    /// Check to see if you can move down.  If you can, then move.  If you
-    /// can't move, throw an InvalidOperationException with the message "Can't go that way!".
-    /// </summary>
     public void MoveDown()
     {
-        // FILL IN CODE
-    }
-
-    public string GetStatus()
-    {
-        return $"Current location (x={_currX}, y={_currY})";
+        if (_mazeMap[(_currX, _currY)][3])
+            _currY--;
+        else
+            throw new InvalidOperationException("Can't go that way!");
     }
 }
--- week03/code/SetsAndMaps.cs
+++ week03/code/SetsAndMaps.cs
@@ -20,32 +20,60 @@ public static class SetsAndMaps
     /// </summary>
     /// <param name="words">An array of 2-character words (lowercase, no duplicates)</param>
     public static string[] FindPairs(string[] words)
+{
+    // Step 1: Put all words into a HashSet for O(1) lookup
+    HashSet<string> set = new HashSet<string>(words);
+
+    // Step 2: Track words we've already used so we don't duplicate pairs
+    HashSet<string> used = new HashSet<string>();
+
+    // Step 3: Store results
+    List<string> results = new List<string>();
+
+    // Step 4: Loop through each word
+    foreach (var word in words)
     {
-        // TODO Problem 1 - ADD YOUR CODE HERE
-        return [];
+        // Skip if already used
+        if (used.Contains(word))
+            continue;
+
+        // Step 5: Reverse the word
+        string reversed = new string(new char[] { word[1], word[0] });
+
+        // Step 6: Check special case (ignore same-letter words like "aa")
+        if (word[0] == word[1])
+            continue;
+
+        // Step 7: Check if reverse exists
+        if (set.Contains(reversed))
+        {
+            results.Add($"{word} & {reversed}");
+            used.Add(word);
+            used.Add(reversed);
+        }
     }
 
-    /// <summary>
-    /// Read a census file and summarize the degrees (education)
-    /// earned by those contained in the file.  The summary
-    /// should be stored in a dictionary where the key is the
-    /// degree earned and the value is the number of people that 
-    /// have earned that degree.  The degree information is in
-    /// the 4th column of the file.  There is no header row in the
-    /// file.
-    /// </summary>
-    /// <param name="filename">The name of the file to read</param>
-    /// <returns>fixed array of divisors</returns>
+    return results.ToArray();
+}
     public static Dictionary<string, int> SummarizeDegrees(string filename)
     {
         var degrees = new Dictionary<string, int>();
         foreach (var line in File.ReadLines(filename))
         {
             var fields = line.Split(",");
-            // TODO Problem 2 - ADD YOUR CODE HERE
-        }
+            var degree = fields[3];
 
-        return degrees;
+            if (degrees.ContainsKey(degree))
+            {
+                degrees[degree]++;
+            }
+            else
+            {
+                degrees[degree] = 1;
+            }
+                    }
+
+                    return degrees;
     }
 
     /// <summary>
@@ -65,10 +93,36 @@ public static class SetsAndMaps
     /// using the [] notation.
     /// </summary>
     public static bool IsAnagram(string word1, string word2)
-    {
-        // TODO Problem 3 - ADD YOUR CODE HERE
+{
+    word1 = word1.Replace(" ", "").ToLower();
+    word2 = word2.Replace(" ", "").ToLower();
+
+    if (word1.Length != word2.Length)
         return false;
+
+    Dictionary<char, int> counts = new Dictionary<char, int>();
+
+    foreach (char c in word1)
+    {
+        if (counts.ContainsKey(c))
+            counts[c]++;
+        else
+            counts[c] = 1;
     }
+
+    foreach (char c in word2)
+    {
+        if (!counts.ContainsKey(c))
+            return false;
+
+        counts[c]--;
+
+        if (counts[c] < 0)
+            return false;
+    }
+
+    return true;
+}
 
     /// <summary>
     /// This function will read JSON (Javascript Object Notation) data from the 
@@ -96,11 +150,19 @@ public static class SetsAndMaps
 
         var featureCollection = JsonSerializer.Deserialize<FeatureCollection>(json, options);
 
-        // TODO Problem 5:
-        // 1. Add code in FeatureCollection.cs to describe the JSON using classes and properties 
-        // on those classes so that the call to Deserialize above works properly.
-        // 2. Add code below to create a string out each place a earthquake has happened today and its magitude.
-        // 3. Return an array of these string descriptions.
-        return [];
+            var results = new List<string>();
+
+    if (featureCollection?.features != null)
+    {
+        foreach (var feature in featureCollection.features)
+        {
+            var place = feature.properties.place;
+            var mag = feature.properties.mag;
+
+            results.Add($"{place} - Mag {mag}");
+        }
+    }
+
+    return results.ToArray();
     }
 }
